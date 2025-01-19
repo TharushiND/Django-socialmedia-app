be10265c40adba31e99ffb582a2ea3a0939d8d61
Tharushi Dharmarathna <tharushindharmarathna@gmail.com>
2025-01-19 19:35:26 +0530

    updated

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class MyUser(AbstractUser):
+    bio = models.CharField(max_length=500)
+    profile_image = models.ImageField(upload_to='profile_image/', blank = True, null=True)
+    
+
