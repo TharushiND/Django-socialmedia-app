8176f9ed55520132329cf2bf0f34cc83e2a6bf8f
Tharushi Dharmarathna <tharushindharmarathna@gmail.com>
2025-01-21 16:17:05 +0530

    Update models.py

--- backend/base/models.py
+++ backend/base/models.py
@@ -2,7 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
+    username= models.CharField(max_length=50, unique=True, primary_key=True)
     bio = models.CharField(max_length=500)
     profile_image = models.ImageField(upload_to='profile_image/', blank = True, null=True)
-    
+    followers = models.models.ManyToManyField("self", symmetrical=false, related_name ='following', blank =True)
+
+    def __str__(self):
+        return self.username
+
+#have to turn this to json format to pass through api
 
